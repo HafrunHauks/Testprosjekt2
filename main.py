691d782343e273c14f72c332d45135ebdd567bfa
Nitrizin <bjovaardal@outlook.com>
2025-10-21 13:01:06 +0200

    Create main.py
    Sted for Brukergrensesnitt

---
+++ main.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+
+tk._test()
+# This code creates a simple Tkinter window to test the Tkinter installation.# When run, it will open a small window with a button to close it.
+
+# To run this code, save it in a file named main.py and execute it with Python.
